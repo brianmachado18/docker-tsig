5116f602257f80235d09254216f02df2ca9b69d7
Rodrigo Fernandez <r.fernandez.mattiaude@gmail.com>
2026-05-18 19:51:54 -0300

    mv src pom -> backend/bk

---
+++ GeoTravel/backend/bk/src/main/java/com/example/tsig/TsigBackendApplication.java
@@ -0,0 +1,11 @@
+package com.example.tsig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsigBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TsigBackendApplication.class, args);
+    }
+}
